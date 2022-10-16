505054c798853c93fabd28c0dc69c07fa8c685b7
Agustin Mellado <aguxmellado@gmail.com>
2022-10-16 17:59:32 -0300

    proceso de autenticacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,25 @@
 import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import config from '../config'
+export const signup = async (req, res) => {
+    const { username, email, password, roles } = req.body;
+    //registro un nuevo usuario
+    const newUser = new User({
+        username,
+        email,
+        //llamo a la funcion encryptPassword
+        password: await User.encryptPassword(password)//guardo la password cifrada.
+    })
+    //guardamos de forma asincrona el nuevo user
+    const savedUser = await newUser.save();
+    //generacion de token
+    const token = jwt.sign({ id: savedUser._id }, config.SECRET, { //ingreso el id del nuevo usuario, la palabra clave
+        //tiempo de expiracion de token
+        expiresIn: 86400 //24hs
+    });
+    res.status(200).json({token})
 
-export const signup= async(req, res)=>{
-    const {username,email,password,roles}=req.body;
-    console.log(req.body);
-    res.json('signup')
 }
-export const signin= async(req, res)=>{
+export const signin = async (req, res) => {
     res.json('signin')
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import { Schema, model } from 'mongoose'
+import bcrypt from 'bcryptjs'
 
-const userSchema=new Schema({
+const userSchema = new Schema({
     username: {
         type: String,
         unique: true
@@ -21,4 +22,19 @@ const userSchema=new Schema({
     timeStamp: true,
     versionKey: false,
 });
-export default userSchema;
+//cifrar password
+//statics permite usar el objeto sin necesitdad de instaciarlo.
+//metodo para cifrar la password
+userSchema.statics.encryptPassword = async (password) => {
+    //.gensalt indica las veces que se aplica el algoritmo.
+    const salt = await bcrypt.genSalt(10);
+    //.hash creo la password cifrada. 
+    return await bcrypt.hash(password, salt)//retorno texto cifrado.
+}
+//metodo para comparar la password
+//tomo la password guardada y la password que el usuario ingresa para login
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+    //.compare devuelve un booleano.
+    return await bcrypt.compare(password, receivedPassword);
+}
+export default model('User',userSchema);
